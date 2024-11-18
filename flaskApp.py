0aedbe779b89dadcd381492464485549147ae324
Aacash Srinath Nambiar <aacash3007@gmail.com>
2024-11-18 22:18:00 +0530

    commit

--- flaskApp.py
+++ flaskApp.py
@@ -9,7 +9,7 @@ ollama = OllamaLLM(model="llama3.2")
 
 
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 
 
@@ -38,7 +38,3 @@ def weekly_fitness_plan():
 
     return parsed_json
 
-
-
-if __name__ == '_main_':
-    app.run(host='0.0.0.0',port = 8080, debug=True)
